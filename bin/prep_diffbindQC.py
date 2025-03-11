117c7c476c3abaec12f51eeb3e1bc1a927329937
Ryan Routsong <routsongrm@nih.gov>
2025-03-11 17:58:27 -0400

    fix: make dirs at script level

--- bin/prep_diffbindQC.py
+++ bin/prep_diffbindQC.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 import argparse
 from csv import DictWriter
-from os.path import basename
+from os.path import basename, dirname, exists
+from os import makedirs
 from itertools import repeat
+
+##
+## Objective : gather all Q5DD bams, their respective controls (if they exist),
+##              and their peaksets together in diffbind-esque csv
+##              see : https://bioconductor.org/packages/release/bioc/manuals/DiffBind/man/DiffBind.pdf
 
 
 def main(args):
@@ -21,15 +27,25 @@ def main(args):
     tbl = {}
     tbl["SampleID"] = list(map(extract_sid, args.sample))
     tbl["Condition"] = list(repeat("", n))
-    tbl["Replicate"] = list(map(str, range(1, n + 1)))
+    tbl["Replicate"] = list(repeat("1", n))
     tbl["bamReads"] = args.sample
     tbl["ControlID"] = list(map(extract_sid, args.control))
     tbl["bamControl"] = args.control
     tbl["Peaks"] = args.peaks
     tbl["PeakCaller"] = list(repeat(args.pktool, n))
-    csv = zip(*[tbl[col] for col in columns])
-    with open(args.output) as csv_out:
-        wrtr = DictWriter(csv_out, columns, delimter=",")
+    csv = []
+    for i in range(n):
+        this_row = {}
+        for col in columns:
+            this_row[col] = tbl[col][i]
+        csv.append(this_row)
+
+    out_dir = dirname(args.output)
+    if not exists(out_dir):
+        makedirs(out_dir, 0o755, exist_ok=True)
+
+    with open(args.output, "w") as csv_out:
+        wrtr = DictWriter(csv_out, columns, delimiter=",")
         wrtr.writeheader()
         for row in csv:
             wrtr.writerow(row)
@@ -37,12 +53,14 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="")
+    parser = argparse.ArgumentParser(
+        description="Script for creating diffbind CSVs for QC purposes"
+    )
     parser.add_argument(
         "-t",
         "--tool",
         dest="pktool",
-        type="string",
+        type=str,
         help="Single string, identify peak tool to diffbind\n"
         + "(see: https://bioconductor.org/packages/release/bioc/manuals/DiffBind/man/DiffBind.pdf)",
     )
@@ -50,10 +68,24 @@ if __name__ == "__main__":
         "-o",
         "--output",
         dest="output",
-        type="string",
+        type=str,
         help="Path to output AllSamples-* csvfile",
     )
-    parser.add_argument("-s", "--samplebams", dest="sample", nargs="+", help="")
-    parser.add_argument("-c", "--controlbams", dest="control", nargs="+", help="")
-    parser.add_argument("-p", "--peaks", dest="peaks", nargs="+", help="")
+    parser.add_argument(
+        "-s",
+        "--samplebams",
+        dest="sample",
+        nargs="+",
+        help="List of the sample BAM files",
+    )
+    parser.add_argument(
+        "-c",
+        "--controlbams",
+        dest="control",
+        nargs="+",
+        help="List of the control BAM files",
+    )
+    parser.add_argument(
+        "-p", "--peaks", dest="peaks", nargs="+", help="List of sample PEAKSETs"
+    )
     main(parser.parse_args())
