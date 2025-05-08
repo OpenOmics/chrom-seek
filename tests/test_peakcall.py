ab59a074e4e2c78dfcae8c54c8ae50967050a0a3
Ryan Routsong <routsongrm@nih.gov>
2025-05-08 14:50:49 -0400

    feat: unit testing and ci for peakcalls

---
+++ tests/test_peakcall.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import unittest
+import shutil
+import tempfile
+import os
+from src.files import peakcalls
+
+
+class TestParsePeakCall(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_full(self):
+        str2file = \
+            "Sample\tInputControl\tGroup\tBlock\n" + \
+            "one\t\tg1\t\n" + \
+            "two\tone\tg2\t\n" + \
+            "three\tone\tg3,g4\ttwo"
+        path = os.path.join(self.test_dir, 'test.txt')
+        with open(path, 'w') as f:
+            f.write(str2file)
+        chip2input, groups, blocks = peakcalls(path)
+        # chips and input mapping
+        self.assertEqual([v for v in chip2input.values()], ['', 'one', 'one'])    
+        # grouping
+        for g in ('g1', 'g2', 'g3', 'g4'):
+            self.assertIn(g, groups.keys())
+        self.assertEqual(groups['g4'], ['three'])
+        # blocking
+        for b in ('one', 'two', 'three'):
+            self.assertIn(b, blocks.keys())
+        self.assertEqual(blocks['two'], '')
+        self.assertEqual(blocks['three'], 'two')
+    
+
+    def test_optional(self):
+        str2file = \
+            "Sample\tGroup\tBlock\n" + \
+            "one\tg1\t\n" + \
+            "two\tg2\t\n" + \
+            "three\tg3,g4\ttwo"
+        path = os.path.join(self.test_dir, 'test.txt')
+        with open(path, 'w') as f:
+            f.write(str2file)
+        chip2input, groups, blocks = peakcalls(path)
+        input_vals = list(set([v for v in chip2input.values()]))
+        # only Nones for input
+        self.assertEqual(input_vals, [None])
+
+
+if __name__ == '__main__':
+    unittest.main()
