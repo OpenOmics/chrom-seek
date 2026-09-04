9a21b43cc0936a7e15bf93a744f6cac5b254845f
Ryan Routsong <ryan.routsong@nih.gov>
2026-09-04 15:48:32 -0500

    fix(jaccard): colorize

--- bin/jaccard_score.py
+++ bin/jaccard_score.py
@@ -14,6 +14,7 @@ single tab-delimited file.
 ##########################################
 # Modules
 import argparse
+import json
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -48,6 +49,7 @@ def loop_jaccard(infileList, genomefile):
     nfiles = len(infileList)
     (colnames, snames) = get_colnames(infileList)
     out = [[1.000] * nfiles for i in range(nfiles)]
+    out2 = pd.DataFrame(out, columns=colnames, index=colnames,dtype="float")
     outTable = []
     for z in range(nfiles):
         fileA = infileList[z]
@@ -55,8 +57,8 @@ def loop_jaccard(infileList, genomefile):
         for y in range(z+1,nfiles):
             fileB = infileList[y]
             (data, keylist) = run_jaccard(fileA, fileB, genomefile)
-            out[z][y] = data[3]
-            out[y][z] = data[3]
+            out[z][y] = float(data[3])
+            out[y][z] = float(data[3])
             if len(outTable) == 0:
                 outTable.append( "\t".join(keylist) )
             outTable.append( "\t".join(data) )
@@ -88,7 +90,26 @@ def get_colnames(infileList):
     return (colnames, snames)
 
 
-def pca_plot(out, snames, peakcaller, pcatabout, outPCAFile):
+def load_sample_groups(config_file):
+    """Load sample-to-group mapping from pipeline config, if available."""
+    if not config_file:
+        return {}
+
+    try:
+        with open(config_file, "r") as handle:
+            cfg = json.load(handle)
+    except (OSError, json.JSONDecodeError):
+        return {}
+
+    groups = cfg.get("project", {}).get("groups", {})
+    sample2group = {}
+    for grp, sample_list in groups.items():
+        for sample in sample_list:
+            sample2group[sample] = grp
+    return sample2group
+
+
+def pca_plot(out, snames, peakcaller, pcatabout, outPCAFile, sample2group=None):
     """
     creates a 2D PCA plot comparing the files based upon jaccard scores
     """
@@ -97,12 +118,21 @@ def pca_plot(out, snames, peakcaller, pcatabout, outPCAFile):
     PCAdata = pd.DataFrame(Y_sklearn, columns=["PC1", "PC2"])
     PCAdata["sample_name"] = snames
     PCAdata["peak_caller"] = peakcaller
+    if sample2group is None:
+        sample2group = {}
+    PCAdata["group"] = [sample2group.get(s, "") for s in snames]
+    use_group_coloring = any([g != "" for g in PCAdata["group"].tolist()])
     PCAdata.to_csv(pcatabout, sep='\t', index=False)
 
     fig, ax = plt.subplots()
-    snames_pal = sns.hls_palette(len(set(snames)),s=.8)
-    sns.set_palette(snames_pal)
-    ax = sns.scatterplot(x="PC1", y="PC2", hue="sample_name", data=PCAdata, s=100)
+    if use_group_coloring:
+        group_pal = sns.hls_palette(len(set(PCAdata["group"])), s=.8)
+        sns.set_palette(group_pal)
+        ax = sns.scatterplot(x="PC1", y="PC2", hue="group", style="sample_name", data=PCAdata, s=100)
+    else:
+        snames_pal = sns.hls_palette(len(set(snames)),s=.8)
+        sns.set_palette(snames_pal)
+        ax = sns.scatterplot(x="PC1", y="PC2", hue="sample_name", data=PCAdata, s=100)
     ax.axhline(y=0, color='grey', linewidth=1,linestyle="--")
     ax.axvline(x=0, color='grey', linewidth=1,linestyle="--")
     ax.set(
@@ -115,15 +145,32 @@ def pca_plot(out, snames, peakcaller, pcatabout, outPCAFile):
     return
 
 
-def plot_heatmap(out, outHeatmapFile, peakcaller, heatmap_tab, snames):
-    snames_pal = sns.hls_palette(len(set(snames)),s=.8)
-    snames_lut = dict(zip(set(snames), snames_pal))
-    snames_cols = pd.Series(snames, index=out.index).map(snames_lut)
+def plot_heatmap(out, outHeatmapFile, peakcaller, heatmap_tab, snames, sample2group=None):
+    if sample2group is None:
+        sample2group = {}
+
+    groups = [sample2group.get(s, "") for s in snames]
+    use_group_coloring = any([g != "" for g in groups])
+
+    if use_group_coloring:
+        group_pal = sns.hls_palette(len(set(groups)), s=.8)
+        group_lut = dict(zip(set(groups), group_pal))
+        row_labels = groups
+        row_lut = group_lut
+        legend_title = "group"
+    else:
+        snames_pal = sns.hls_palette(len(set(snames)),s=.8)
+        snames_lut = dict(zip(set(snames), snames_pal))
+        row_labels = snames
+        row_lut = snames_lut
+        legend_title = "sample"
+
+    snames_cols = pd.Series(row_labels, index=out.index).map(row_lut)
     g = sns.clustermap(out, cmap="YlGnBu", col_cluster=False, row_colors=snames_cols)
-    for label in set(snames):
-        g.ax_col_dendrogram.bar(0, 0, color=snames_lut[label],
+    for label in set(row_labels):
+        g.ax_col_dendrogram.bar(0, 0, color=row_lut[label],
                         label=label, linewidth=0)
-    g.ax_col_dendrogram.legend(loc="center", ncol=3, 
+    g.ax_col_dendrogram.legend(title=legend_title, loc="center", ncol=3, 
                             bbox_to_anchor=(0.5, 0.8))
     plt.savefig(outHeatmapFile, bbox_inches='tight')
     plt.close("all")
@@ -201,6 +248,14 @@ def main():
         required=True,
         help='The genome contig sizes reference file'
     )
+    parser.add_argument(
+        '-c',
+        '--config',
+        dest='config_file',
+        required=False,
+        default=None,
+        help='Pipeline config.json file for group annotations'
+    )
 
     args = parser.parse_args()
 
@@ -213,9 +268,11 @@ def main():
     outHeatmapFile = args.heatmap
     pkcaller = args.peakcaller
     hm_tsv = args.heatmap_tab
+    config_file = args.config_file
 
     # downstream processing
     infileList = split_infiles(infiles)
+    sample2group = load_sample_groups(config_file)
 
     outTable, out, snames = loop_jaccard(infileList, genomefile)
     write_out(
@@ -227,14 +284,16 @@ def main():
         snames,
         pkcaller,
         outPCAtab, 
-        outPCAplot
+        outPCAplot,
+        sample2group=sample2group
     )
     plot_heatmap(
         out, 
         outHeatmapFile,
         pkcaller,
         hm_tsv,
-        snames
+        snames,
+        sample2group=sample2group
     )
 
 if __name__ == '__main__':
